0b30fc0ea46fbe5e14f7342e57d724dd394a51ff
AsMurphy-student <AsMurphy-work@protonmail.com>
2025-09-16 11:22:11 -0400

    Started assignment with page loading with css

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const htmlHandler = require('./htmlResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, response) => {
+    switch (request.url) {
+        case '/':
+            htmlHandler.getIndex(request, response);
+            break;
+        case '/style.css':
+            htmlHandler.getCss(request, response);
+            break;
+    }
+};
+
+http.createServer(onRequest).listen(port, () => {
+    console.log(`Listening on port 127.0.0.${port}`);
+});
